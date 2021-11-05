6089207b3074f4ab3a3c7a8e8ef53d7d3bb096c9
jimenarw <jimena.witen@gmail.com>
2021-11-04 22:59:24 -0300

    migrando a reactjs completado

---
+++ src/components/TopBar.js
@@ -0,0 +1,17 @@
+import TopBarNavBar from "./TopBarNavBar";
+
+function TopBar() {
+    return ( 
+        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+					
+            <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+                <i className="fa fa-bars"></i>
+            </button>
+
+            <TopBarNavBar/>
+
+		</nav>
+     );
+}
+
+export default TopBar;
